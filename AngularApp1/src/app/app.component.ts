a01c6dad95ba1062106cdc161da63599f5c72387
shivali umbarkar <shivali.umbarkar@konverge.ai>
2022-04-13 19:10:42 +0530

    updated

---
+++ AngularApp1/src/app/app.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  // 'uplatz',
+  // .app-root   class
+  // [app-root]  attribute
+  // div[app=app1]    attribute +value
+  templateUrl: './app.component.html',
+  // inline template :
+  // ` <h1 style="color:blue">Welcome to shiva game  </h1>
+  //   <h2 style="color:red">Welcome to aditya  game  </h2> `,2
+  styleUrls: ['./app.component.css'],
+
+  // ` h1{color:green}
+  //   h1{font-style:italic}
+  //   h1{background-color:yellow}
+  // `
+})
+export class AppComponent {
+  public title: string = 'Welcome to konverge.ai';
+  public message: string = 'Welcome to Node-Js with shivali ';
+  public d1 = new Date();
+  public img_loc = './assets/lion.jpg';
+}
