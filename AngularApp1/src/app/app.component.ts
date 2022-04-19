af65d6ca94e0331935bbad298df63ea994937aaa
shivali umbarkar <shivali.umbarkar@konverge.ai>
2022-04-19 12:05:53 +0530

    added pipes

--- AngularApp1/src/app/app.component.ts
+++ AngularApp1/src/app/app.component.ts
@@ -21,5 +21,22 @@ export class AppComponent {
   public title: string = 'Welcome to konverge.ai';
   public message: string = 'Welcome to Node-Js with shivali ';
   public d1 = new Date();
-  public img_loc = './assets/lion.jpg';
+  // public img_loc = './assets/lion.jpg';
+  num = 100;
+  f1() {
+    return 'welcome to f1() method ..... ';
+  }
+  // defining function
+  getData() {
+    console.warn('hello world');
+  }
+  getname() {
+    alert('hello konverge');
+  }
+  getname1(name1: string) {
+    alert(name1);
+  }
+  getname2(name: any) {
+    alert(name);
+  }
 }
